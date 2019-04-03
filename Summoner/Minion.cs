629311924a50e820caf1f6b7f0c0b18dbce95b3d
Abdallah rahal <abdalah.rahal15@gmail.com>
2019-04-03 13:50:25 +0200

    ajout solution

--- Summoner/Minion.cs
+++ Summoner/Minion.cs
@@ -22,6 +22,17 @@ namespace Trinity
         Legging legging;
         Boot boot;
 
+        internal Minion ( string name, uint power, uint max_life_point, uint max_mana_point, uint dodge_rate, uint accuracy )
+        {
+            _name = name;
+            _life_point = _max_life_point = max_life_point;
+            _mana_point = _max_mana_point = max_mana_point;
+            _power = power;
+            _dodge_rate = dodge_rate;
+            _accuracy = accuracy;
+            _ratio = 1;
+
+        }
 
         public string Name
         {
@@ -33,40 +44,52 @@ namespace Trinity
             get { return _life_point; }
             set { _life_point = value; }
         }
+
         public uint Max_life_point
         {
             get { return _max_life_point; }
             set { _max_life_point = value; }
         }
+
         public uint Power
         {
             get { return _power; }
             set { _power = value; }
         }
+
         public uint Mana_point
         {
             get { return _mana_point; }
             set { _mana_point = value; }
         }
+
         public uint Max_mana_point
         {
             get { return _max_mana_point; }
             set { _max_mana_point = value; }
         }
+
         public uint Dodge_rate
         {
             get { return _dodge_rate; }
             set { _dodge_rate = value; }
         }
+
         public uint Accuracy
         {
             get { return _accuracy; }
             set { _accuracy = value; }
         }
+
         public uint Ratio
         {
             get { return _ratio; }
             set { _ratio = value; }
+        }
+
+        public bool is_alive()
+        {
+          return (_life_point > 0) ?  true :  false;
         }
     }
 }
