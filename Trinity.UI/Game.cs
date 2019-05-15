a58b7b5685a5936fa5463d6b9fb08e3d4ec2e39a
MorganMBA <mba@intechinfo.fr>
2019-05-15 10:16:09 +0200

    weaponry

--- Trinity.UI/Game.cs
+++ Trinity.UI/Game.cs
@@ -17,27 +17,13 @@ namespace Trinity.UI
         static Inventory_UI inventory_UI = new Inventory_UI(Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/Inventory.png"), summoner, window);
         static Store_UI story_UI = new Store_UI(Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/shop.png"), tower, window);
         bool Onfight = false;
+        static Weaponry warriors = tower.Weaponry;
 
         public void Start()
         {
-            Weapon arc = tower.Equipement_Collection.Create_Weapon("Arc de Ryan", 10, 50, Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/arc.png"));
-            Weapon epee = tower.Equipement_Collection.Create_Weapon("epee de Ryan", 10, 50, Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/epee.png"));
-            Hat bob = tower.Equipement_Collection.Create_Hat("bob de morgan", 50, 10, 20, 2, 4, Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/bob.jpg"));
-            Boots lv = tower.Equipement_Collection.Create_Boots("lv", 5, 10, 10, 20, 4, Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/boot1.png"));
-            Boots nike = tower.Equipement_Collection.Create_Boots("nike", 5, 10, 10, 20, 4, Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/boot2.png"));
-            Hat carre = tower.Equipement_Collection.Create_Hat("Carré rouge", 50, 10, 20, 2, 4, Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/chapeau.png"));
-            Breastplate ocho = tower.Equipement_Collection.Create_Breastplate("quavo", 10, 10, 10, 10, 10, Path.Combine(Directory.GetCurrentDirectory(), "../../../Sprites/quavo.jpg"));
-            //Leg jambiere = tower.Equipement_Collection.Create_Leg("Jambier des z'homme", 1000, 10,50,20,50,)
-            Minion minion1 = tower.Minion_Collection.Create_Minion("Morgan", 100, 100, 100, 20, 20, Path.Combine(Directory.GetCurrentDirectory(), "../../../MinionSprites/Morgan.png"));
-            Minion minion2 = tower.Minion_Collection.Create_Minion("Mergi", 50, 23, 65, 0, 20, Path.Combine(Directory.GetCurrentDirectory(), "../../../MinionSprites/Morgan.png"));
 
             tower.Store.Aviable();
-            List<Equipement> ma_liste = tower.Store.Aviable_Equipement;
             
-
-            summoner.Inventory.Attach_Minons(minion1);
-            summoner.Inventory.Attach_Minons(minion2);
-
 
             window.SetFramerateLimit(60);
 
