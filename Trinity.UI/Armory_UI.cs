afeab3a9fe98f986d01596e47098adc4b248bf56
Butkeim <yanis.kerrad@epitech.eu>
2019-05-17 20:23:32 +0200

    Sprites

---
+++ Trinity.UI/Armory_UI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using SFML.Audio;
+
+namespace Trinity.UI
+{
+    class Armory_UI
+    {
+        public float Xpos;
+        public float Ypos;
+        Sprite sprite;
+        Texture texture;
+        bool _draw;
+        Tower _context;
+        Item _item;
+
+        public Armory_UI(string filename, Summoner summoner, RenderWindow window)
+        {
+
+            texture = new Texture(filename);
+            sprite = new Sprite(texture);
+            sprite.Scale = new Vector2f((float)window.Size.X / 1700f, (float)window.Size.Y / 900f);
+            _draw = false;
+
+        }
+        public void Draw(RenderWindow window)
+        {
+            _draw = true;
+            window.Draw(sprite);
+        }
+    }
+}
