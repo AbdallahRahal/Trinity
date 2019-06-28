8752ec8bf0ae9f989e96364a5d41c08b0fb57e46
Abdallah rahal <abdalah.rahal15@gmail.com>
2019-06-28 15:30:02 +0200

    classe des sort

---
+++ Summoner/Electrocute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinity
+{
+    class Electrocute
+    {
+        Gem gem;
+
+        public Electrocute(Gem _gem) 
+        {
+            gem = _gem;
+        }
+
+        public void action(List<Minion> Fighters ,Minion focus, Minion focusheal) { 
+            foreach (Minion min in Fighters)
+            {
+                if (focus == min)
+                {
+                    //bubbleFight.Change_type(15, minionPos[minionAction.targetMin]);
+                }
+                else
+                {
+                    //bubbleFight.Change_type(8, minionPos[minionAction.targetMin]);
+                }
+            }
+        }
+        public string description()
+        {
+            return " Electrocute tout les ennemies, inflige plus de degats au focus";
+        }
+        public void update()
+        {
+
+        }
+    }
+}
---
+++ Summoner/Heal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinity
+{
+    class Heal
+    {
+        Gem gem;
+
+        public Heal(Gem _gem)
+        {
+            gem = _gem;
+        }
+
+        public void action(List<Minion> Fighters, Minion focus, Minion focusheal)
+        {
+            if (focusheal != null)
+            {
+                //soigne de 20 sur focus heal
+            }
+            else
+            {
+                Minion minheal;
+                int lp = 0;
+                foreach (Minion min in Fighters)
+                {
+                    if (min._summoner == this.gem.wearer._summoner)
+                    {
+                        if (min.Life_point > lp)
+                        {
+                            minheal;
+                        }
+
+                    }
+                }
+            }
+        }
+            public void update()
+        {
+
+        }
+
+        public string description()
+        {
+            return " Soigne le focus allié si il existe, sinon soigne l'allié avec le plus bas taux de point de vie";
+        }
+    }
+}
